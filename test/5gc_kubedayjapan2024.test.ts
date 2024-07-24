4793ff9ffd8ee6043acde7f4c7a8257e22cd6576
mgonzalezo <mgonzalezortiz41@gmail.com>
2024-07-24 14:58:24 +0900

    EKS blueprint creation

---
+++ test/5gc_kubedayjapan2024.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as 5GcKubedayjapan2024 from '../lib/5gc_kubedayjapan2024-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/5gc_kubedayjapan2024-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new 5GcKubedayjapan2024.5GcKubedayjapan2024Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
